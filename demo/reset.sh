798a0bbcea44aec462a966e0fb1ab8e165013265
Robert Bittle <guywithnose@gmail.com>
2014-11-14 16:57:30 -0500

    Update slides

--- demo/reset.sh
+++ demo/reset.sh
@@ -4,4 +4,4 @@ docker build -t my-nginx .
 
 docker run -d --name demo-mysql -e MYSQL_ROOT_PASSWORD=password mysql
 docker run -d -v $(pwd):/srv/http --name demo-phpfpm --link demo-mysql:mysql jprjr/php-fpm
-docker run -d -v $(pwd):/usr/local/nginx/html --link demo-phpfpm:phpfpm -p 81:80 my-nginx
+docker run -d -v $(pwd):/usr/local/nginx/html --link demo-phpfpm:phpfpm -p 80:80 my-nginx
