cb5e1bec57e084b4cdea28100d3489cdc50b7bb2
Robert Bittle <guywithnose@gmail.com>
2014-11-21 16:51:39 -0500

    Update docker

--- demo/reset.sh
+++ demo/reset.sh
@@ -3,5 +3,5 @@ docker ps|awk '{print $1}'|grep -v CONTAINER|xargs docker rm -f #Remove all runn
 docker build -t my-nginx .
 
 docker run -d --name demo-mysql -e MYSQL_ROOT_PASSWORD=password mysql
-docker run -d -v $(pwd):/srv/http --name demo-phpfpm --link demo-mysql:mysql jprjr/php-fpm
+docker run -d -v $(pwd):/srv/http -v $(pwd)/timezone.ini:/etc/php/conf.d/timezone.ini --name demo-phpfpm --link demo-mysql:mysql jprjr/php-fpm
 docker run -d -v $(pwd):/usr/local/nginx/html --link demo-phpfpm:phpfpm -p 80:80 my-nginx
